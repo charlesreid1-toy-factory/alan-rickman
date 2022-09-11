fba05c0a1b5ea8862be6527b915d72f211bd7cd0
Charles Reid <charlesreid1@gmail.com>
2022-09-11 11:13:18 -0700

    update

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from src import __version__
+
+with open("requirements.txt") as f:
+    all_deps = [x for x in f.read().splitlines() if not x.startswith("#")]
+with open("requirements-dev.txt") as f:
+    dev_deps = [x for x in f.read().splitlines() if not x.startswith("#")]
+
+setup(
+    name="alan-rickman",
+    version=__version__,
+    description="",
+    author="Chaz Reid",
+    author_email="charlesreid1@gmail.com",
+    url="https://github.com/charlesreid1-toy-factory/alan-rickman",
+    packages=["alan_rickman"],
+    package_dir={"alan_rickman": "src"},
+    license="MIT",
+    install_requires=all_deps,
+    extras_require={
+        "test": dev_deps,
+        "docs": dev_deps,
+    }
+)
+
